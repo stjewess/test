067fe8e501ebb6d7da5e032efa84aa3f2883c8b2
София <@SOFY>
2023-02-01 22:54:47 +0300

    Добавьте файлы проекта.

---
+++ Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            var nado = BazaEntities1.GetContext().character.ToList();
+            List.ItemsSource = nado;
+        }
+
+        private void update_page()
+        {
+            var current_character = BazaEntities1.GetContext().character.ToList();
+
+            current_character = current_character.Where(p => p.name.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            List.ItemsSource = current_character;
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            update_page();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ vkhod.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// Логика взаимодействия для vkhod.xaml
+    /// </summary>
+    public partial class vkhod : Page
+    {
+        public vkhod()
+        {
+            InitializeComponent();
+        }
+
+        private void start_click(object sender, RoutedEventArgs e)
+        { 
+        Manager.MainFrame.Navigate(new Page1());
+            var nado = new BazaEntities1();
+            var Base = nado.clients.Where(u => u.login == Log.Text.ToLower() && u.password == Pass.Text.ToLower()).FirstOrDefault();
+            if (Base != null)
+            {
+                Manager.MainFrame.Navigate(new Page1());
+            }
+            else
+            {
+                MessageBox.Show("Нужно войти в учётную запись!");
+                Manager.MainFrame.Navigate(new vkhod());
+            }
+        }
+    }
+}
